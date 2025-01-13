d245b62f36b8a1607e36594f9c8ba462c5482f71
James Natulan <jamesnatulan@gmail.com>
2025-01-13 12:20:15 +0800

    Multiagent system

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ from smolagents import (
     CodeAgent,
     DuckDuckGoSearchTool,
     VisitWebpageTool,
+    ManagedAgent,
+    LiteLLMModel
 )
 
 from retriever import RetrieverTool
@@ -15,8 +17,12 @@ def main():
     print("Hello from agentic-rag!")
 
     # Model
-    model_id = "Qwen/Qwen2.5-Coder-32B-Instruct"
-    model = HfApiModel(model_id)
+    # model_id = "Qwen/Qwen2.5-Coder-32B-Instruct"
+    # model = HfApiModel(model_id)
+    model = LiteLLMModel(
+        "ollama/qwen2.5-coder:1.5b",
+        api_base="http://localhost:11434"
+    )
 
     # Load the knowledge-base
     # TODO: Uses transformers library of markdown files, can be changed with other scraped docs
@@ -42,14 +48,50 @@ def main():
     docs_processed = text_splitter.split_documents(source_docs)
     retriever_tool = RetrieverTool(docs_processed)
 
-    # Initialize the Agent
-    agent = CodeAgent(
-        tools=[retriever_tool, DuckDuckGoSearchTool(), VisitWebpageTool()],
+    # Create retriever Agent
+    retriever_agent = CodeAgent(
+        tools=[retriever_tool],
+        model=model,
+        max_steps=4,
+        verbose=True,
+    )
+    retriever_agent = ManagedAgent(
+        retriever_agent,
+        name="retriever_agent",
+        description="""Retrieves relevant documents from the knowledge base based on the input question""",
+    )
+
+    # Create web search Agent
+    web_search_agent = CodeAgent(
+        tools=[DuckDuckGoSearchTool(), VisitWebpageTool()],
         model=model,
         max_steps=10,
         verbose=True,
-        additional_authorized_imports=["time", "numpy", "pandas"],
     )
+    web_search_agent = ManagedAgent(
+        web_search_agent,
+        name="web_search",
+        description="""Runs web searches only to append, verify, or fill in missing information from
+        the retrieved documents""",
+    )
+
+    # Create the manager agent
+    manager_agent = CodeAgent(
+        tools=[],
+        model=model,
+        managed_agents=[retriever_agent, web_search_agent],
+        max_steps=5,
+        additional_authorized_imports=["time", "numpy", "pandas"],
+        verbose=True,
+    )
+    # # Initialize the Agent
+    # agent = CodeAgent(
+    #     tools=[retriever_tool, DuckDuckGoSearchTool(), VisitWebpageTool()],
+    #     model=model,
+    #     max_steps=10,
+    #     verbose=True,
+    #     additional_authorized_imports=["time", "numpy", "pandas"],
+    # )
 
     # Run sample
     prompt_template = """
@@ -60,7 +102,7 @@ def main():
 
     question = input("Ask me anything!: ")
 
-    agent_output = agent.run(
+    agent_output = manager_agent.run(
         prompt_template.format(question=question)
     )
 
